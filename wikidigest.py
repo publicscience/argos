1d6f93bed524b47735a29af3a3c59f32f2b9e790
Francis Tseng <ftzeng@gmail.com>
2013-08-04 13:02:42 -0400

    added naive mediawiki clean func

--- wikidigest.py
+++ wikidigest.py
@@ -4,20 +4,35 @@ DUMP_URL = 'http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-stub-articles
 DATA_PATH = '../data/wiki/'
 NAMESPACE = 'http://www.mediawiki.org/xml/export-0.8/'
 
+import string
 from digester import Digester, Adipose
-import re
+from mwlib import parser
+from mwlib.refine.compat import parse_txt
 
 def main():
     d = Digester('data/wiki/article.xml', NAMESPACE)
     #a = Adipose('test', 'titles')
 
-    def process_element(elem):
-        print elem
-        #data = {'title': elem.find('{%s}title' % NAMESPACE).text.encode('utf-8')}
+    def clean(text):
+        """
+        Cleans up MediaWiki text of markup.
+        Currently a 'naive' version in that
+        it just strips *all* punctuation.
 
-        # This extracts the page link text (i.e. the text that links to other pages) from an article.
+        Will eventually want to strip out unnecessary
+        markup syntax as well, such as "File:" and
+        "Category".
+
+        Args:
+            | text (str)    -- the MediaWiki text to cleanup. Must be utf-8 encoded.
+        """
+        replace_punctuation = string.maketrans(string.punctuation, ' '*len(string.punctuation))
+        return text.translate(replace_punctuation)
+
+    def process_element(elem):
         text = elem.find('{%s}revision' % NAMESPACE).find('{%s}text' % NAMESPACE).text.encode('utf-8')
-        print re.findall('(?<=\[\[)[^a-z*:|File:][^\]|#]*(?=\|)|(?<=\[\[)[^a-z*:|File:][^\]|#]*(?=\]\])', text)
+
+        print clean(text)
 
         #a.empty()
         #a.add(data)
