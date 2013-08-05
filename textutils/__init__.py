634d997e0f1c923b322ca79a51be43e3c936c3fb
Francis Tseng <ftzeng@gmail.com>
2013-08-05 16:03:20 -0400

    moving text utilities to their own package

---
+++ textutils/__init__.py
@@ -0,0 +1,40 @@
+"""
+Text
+==============
+
+Text processing utilities.
+"""
+
+from HTMLParser import HTMLParser
+import string
+
+def trim(text):
+    """
+    Compresses and trims extra whitespace.
+    """
+    return ' '.join(text.split())
+
+def depunctuate(text):
+    """
+    Removes all punctuation from text,
+    replacing them with spaces.
+    """
+    replace_punctuation = string.maketrans(string.punctuation, ' '*len(string.punctuation))
+    return text.translate(replace_punctuation)
+
+def sanitize(html):
+    """
+    Strips HTML markup.
+    """
+    s = Sanitizer()
+    s.feed(html)
+    return s.get_data()
+
+class Sanitizer(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
