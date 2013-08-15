6de72c4573fdf46096a44b072d3da46a5f376b80
Francis Tseng <ftzeng@gmail.com>
2013-08-14 21:45:56 -0400

    fixed test for freq counting, added test for named entity recognition

--- tests/brain_test.py
+++ tests/brain_test.py
@@ -16,11 +16,43 @@ class BrainTest(unittest.TestCase):
     def test_simple_count(self):
         data = "hey there buddy, hey"
         freqs = dict(self.b.count(data))
-        self.assertEqual(freqs, {
-            'hey': 2,
-            'there': 1,
-            'buddy': 1
-            })
+
+        # 'there' is filtered out as a stopword.
+        expected = {
+                'hey': 2,
+                'buddy': 1
+        }
+        self.assertEqual(freqs, expected)
+
+    def test_entity_recognition(self):
+        with open('tests/data/sample.txt', 'r') as f:
+            sample = f.read()
+        results = self.b.recognize(sample)
+
+        expected = {
+                'Trinity',
+                'Julius Robert Oppenheimer',
+                'California',
+                'New Mexico',
+                'American',
+                'Berkeley',
+                'Advanced Study',
+                'Enrico Fermi',
+                'Soviet Union',
+                'Princeton',
+                'Johnson',
+                'Oppenheimer',
+                'Bhagavad Gita',
+                'United States Atomic Energy Commission',
+                'Lyndon',
+                'Manhattan Project',
+                'Enrico Fermi Award',
+                'Second Red Scare',
+                'University',
+                'John'
+        }
+
+        self.assertEqual(results, expected)
 
 if __name__ == '__main__':
 	unittest.main()
