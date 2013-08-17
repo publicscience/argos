394a8a81f4fc45af993c3dbafbff29549d9a405d
Francis Tseng <ftzeng@gmail.com>
2013-08-16 20:13:34 -0400

    wikidigester profiler

--- profile.py
+++ profile.py
@@ -1,10 +1,10 @@
-import cProfile
+import cProfile, pstats
 from wikidigester import WikiDigester
 from adipose import Adipose
 
 def profile_wikidigester():
     # Create a WikiDigester
-    w = WikiDigester('data/wiki/wiki.xml', 'pages')
+    w = WikiDigester('data/wiki/wiki_profile.xml', 'pages')
 
     # Setup a database.
     db = Adipose('test', 'pages')
@@ -14,7 +14,15 @@ def profile_wikidigester():
     w.db = db
 
     print('Profiling WikiDigester...')
-    cProfile.run(w.digest())
+    #cProfile.runctx('w.digest()', None, {'w': w})
+
+    p = cProfile.Profile()
+    p.runctx('w.digest()', None, {'w': w})
+    ps = pstats.Stats(p)
+
+    # Print the top 10 functions that take the most time.
+    ps.strip_dirs().sort_stats('time').print_stats(10)
+
 
 if __name__ == '__main__':
     profile_wikidigester()
