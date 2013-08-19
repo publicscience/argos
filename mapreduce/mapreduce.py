efac37c9c9b8467315ac63c91001d7f98c5dfeca
Francis Tseng <ftzeng@gmail.com>
2013-08-19 01:21:58 -0400

    fleshing out mapreduce

--- mapreduce/mapreduce.py
+++ mapreduce/mapreduce.py
@@ -1,17 +1,27 @@
 #!../mr-env/bin/python
 
 from mrjob.job import MRJob
-import re
-
-WORD_RE = re.compile(r"[\w']+")
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
 
 class MRWordFreqCount(MRJob):
     def mapper_init(self):
-        pass
+        self.lemmr = WordNetLemmatizer()
+        self.stops = list(string.punctuation) + stopwords.words('english')
 
     def mapper(self, key, line):
-        for word in WORD_RE.findall(line):
-            yield (word.lower(), 1)
+        # Tokenize
+        for sentence in sent_tokenize(line):
+            for word in word_tokenize(sentence):
+                # Skip stopwords
+                if word in self.stops:
+                    continue
+
+                # Lemmatize
+                lemma = self.lemmr.lemmatize(word.lower())
+                yield (lemma, 1)
 
     def combiner(self, word, counts):
         yield (word, sum(counts))
