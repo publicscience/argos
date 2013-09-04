3a31deea266b6e9f2221c9e9c7105d862ac10f3d
Francis Tseng <ftzeng@gmail.com>
2013-09-03 22:26:26 -0400

    setup image script should be working now

--- cluster/setup_image.sh
+++ cluster/setup_image.sh
@@ -8,5 +8,18 @@ sudo apt-get install salt-minion -y
 sudo apt-get install salt-master -y
 sudo apt-get upgrade -y
 
-# Start Minion
-salt-minion -d
+# Enable fileserver at /srv/salt
+# and enable pillar at /srv/pillar
+# This is necessary to run a masterless minion.
+sudo sed -i '/#\(file\|pillar\)_roots:/ s/^#//' /etc/salt/minion
+sudo sed -i '/#\s\{2\}base:/ s/^#//' /etc/salt/minion
+sudo sed -i '/#\s\{4\}\-\s\/srv\/\(salt\|pillar\)/ s/^#//' /etc/salt/minion
+
+# Wait for the proper files to show up.
+while [ ! -f /srv/salt/digester/init.sls ]
+do
+    sleep 10
+done
+
+# Provision as a masterless minion.
+sudo salt-call state.highstate --local
