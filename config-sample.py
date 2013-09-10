6813990cd945bf7630b9e8d7ec1926b66efbbbd0
Francis Tseng <ftzeng@gmail.com>
2013-09-10 11:57:12 -0400

    added config sample

---
+++ config-sample.py
@@ -0,0 +1,8 @@
+# Database config.
+DB_HOST = 'localhost'
+
+# Email config.
+EMAIL_SERVER = ('smtp.gmail.com', 587)
+EMAIL_USER = 'shallow.thought.bot@gmail.com'
+EMAIL_PASS = 'YOUR PASS'
+EMAIL_TARGETS = ['ftzeng@gmail.com']
