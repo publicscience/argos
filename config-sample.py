2e0aa575b7fd83c0a1981cf3c1b4401863c79983
Francis Tseng <ftzeng@gmail.com>
2013-09-10 13:32:08 -0400

    config shuffle

--- config-sample.py
+++ config-sample.py
@@ -1,7 +1,2 @@
 # Database config.
 DB_HOST = 'localhost'
-
-# Email config.
-EMAIL_SERVER = ('smtp.gmail.com', 587)
-EMAIL_LOGIN = ('shallow.thought.bot@gmail.com', 'YOUR PASS')
-EMAIL_TARGETS = ['ftzeng@gmail.com']
