08eeb3d4f732f8a50542779124ba1024a4ce1ad9
Ferris Tseng <ferristseng@gmail.com>
2014-01-30 00:36:21 -0500

    added User, Role models, and a massive configuration file for Flask-Security

--- conf/development_app.py
+++ conf/development_app.py
@@ -1,3 +1,5 @@
+from conf.base_security import *
+
 # App config
 
 SQLALCHEMY_DATABASE_URI = "{DATABASE[default][TYPE]}://{DATABASE[default][HOST]}:{DATABASE[default][PORT]}/{DATABASE[default][NAME]}"
