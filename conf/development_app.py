09497ae03d61109cf41c49b67092eaa61f3a6a0a
Ferris Tseng <ferristseng@gmail.com>
2014-01-30 01:12:08 -0500

    updated conf for development

--- conf/development_app.py
+++ conf/development_app.py
@@ -1,5 +1,13 @@
 from conf.base_security import *
 
-# App config
+# App Config
 
 SQLALCHEMY_DATABASE_URI = "{DATABASE[default][TYPE]}://{DATABASE[default][HOST]}:{DATABASE[default][PORT]}/{DATABASE[default][NAME]}"
+
+# Security Config
+
+SECURITY_SEND_REGISTER_EMAIL = False 
+SECURITY_SEND_PASSWORD_CHANGE_EMAIL = False 
+SECURITY_SEND_PASSWORD_RESET_NOTICE_EMAIL = False 
+
+
