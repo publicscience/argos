91f05c5dd923eb6799e8c213e6cde3cc69b18f39
Francis Tseng <ftzeng@gmail.com>
2014-02-19 19:04:43 -0500

    seed data now seeds with some random remote images for articles

--- manage/seed/__init__.py
+++ manage/seed/__init__.py
@@ -14,10 +14,24 @@ import os, json
 from datetime import datetime
 from dateutil.parser import parse
 
+import random
+
 def seed(debug=False):
     this_dir = os.path.dirname(__file__)
     seeds = open(os.path.join(this_dir, 'seed.json'), 'r')
     sources = open(os.path.join(this_dir, 'seed_sources.json'), 'r')
+
+    sample_images = [
+        'https://upload.wikimedia.org/wikipedia/commons/d/d5/Michael_Rogers_-_Herbiers_2004.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/6/6e/Brandenburger_Tor_2004.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/ChicagoAntiGaddafiHopeless.jpg/576px-ChicagoAntiGaddafiHopeless.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Evo_morales_2_year_bolivia_Joel_Alvarez.jpg/640px-Evo_morales_2_year_bolivia_Joel_Alvarez.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/2010_wet_season_cloud_over_colombia.jpg/640px-2010_wet_season_cloud_over_colombia.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Barack_Obama_at_Las_Vegas_Presidential_Forum.jpg/640px-Barack_Obama_at_Las_Vegas_Presidential_Forum.jpg'
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/2010-10-23-Demo-Stuttgart21-Befuerworter.png/640px-2010-10-23-Demo-Stuttgart21-Befuerworter.png',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/51%C2%BA_Congresso_da_UNE_%28Conune%29.jpg/640px-51%C2%BA_Congresso_da_UNE_%28Conune%29.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Tough_return%2C_365.35.jpg/640px-Tough_return%2C_365.35.jpg'
+    ]
 
     print('Resetting the database...')
     db.drop_all()
@@ -51,7 +65,7 @@ def seed(debug=False):
                 title=entry['title'],
                 created_at = parse(entry['published']),
                 updated_at = parse(entry['updated']),
-                image='http://frnsys.com/img/projects/album-art.png' # fake image
+                image=random.choice(sample_images) # fake image
         )
         articles.append(a)
         db.session.add(a)
@@ -66,13 +80,13 @@ def seed(debug=False):
     print('Found {0} entities.'.format(num_entities))
 
     print('Clustering articles into events...')
-    Event.cluster(articles, threshold=0.04, debug=True)
+    Event.cluster(articles, threshold=0.02, debug=True)
     num_events = Event.query.count()
     print('Created {0} event clusters.'.format(num_events))
 
     print('Clustering events into stories...')
     events = Event.query.all()
-    Story.cluster(events, threshold=0.04, debug=True)
+    Story.cluster(events, threshold=0.02, debug=True)
     num_stories = Story.query.count()
     print('Created {0} story clusters.'.format(num_stories))
 
