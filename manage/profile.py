ef6637f031fb2bbc7997966c158f00d328f94a87
Francis Tseng <ftzeng@gmail.com>
2014-03-17 00:04:45 -0400

    expanded profiling a bit, see #25

--- manage/profile.py
+++ manage/profile.py
@@ -7,7 +7,14 @@ and identify bottlenecks.
 """
 
 import cProfile, pstats
-from digester.wikidigester import WikiDigester
+
+from argos.core.digester.wikidigester import WikiDigester
+
+from argos.datastore import db
+
+from argos.core.brain import vectorize
+from argos.core.brain.summarize import summarize, multisummarize
+from argos.core.models import Article, Event
 
 def profile_wikidigester():
     # Create a WikiDigester
@@ -25,5 +32,41 @@ def profile_wikidigester():
     # See which specific func takes the most time.
     ps.sort_stats('time').print_stats(10)
 
+def profile_vectorize():
+    print('argos.core.brain.vectorize')
+    doc = open('tests/data/multidoc/1.txt', 'r').read()
+    profile_cmd('v(d)', None, {'v': vectorize, 'd': doc})
+
+def profile_summarize():
+    print('argos.core.brain.summarize.summarize')
+    doc = open('tests/data/multidoc/1.txt', 'r').read()
+    title = 'This is a title'
+    profile_cmd('s(t, d)', None, {'s': summarize, 't': title, 'd': doc})
+
+def profile_multisummarize():
+    print('argos.core.brain.summarize.multisummarize')
+    docs = [open('tests/data/multidoc/{0}.txt'.format(i), 'r').read() for i in range(1,4)]
+    profile_cmd('s(d)', None, {'s': multisummarize, 'd': docs})
+
+def profile_event_clustering():
+    print('argos.core.models.event.Event.cluster')
+    docs = [open('tests/data/multidoc/{0}.txt'.format(i), 'r').read() for i in range(1,4)]
+    articles = [Article(title='Test title', text=doc) for doc in docs]
+    db.session.add_all(articles)
+    db.session.commit()
+    profile_cmd('E.cluster(a)', None, {'E': Event, 'a': articles})
+
 def profile():
-    profile_wikidigester()
+    profile_vectorize()
+    profile_summarize()
+    profile_multisummarize()
+    profile_event_clustering()
+
+
+def profile_cmd(*args):
+    p = cProfile.Profile()
+    p.runctx(*args)
+    ps = pstats.Stats(p)
+
+    # See which specific func takes the most time.
+    ps.sort_stats('time').print_stats(10)
