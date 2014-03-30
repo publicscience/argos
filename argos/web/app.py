27bbafd3de29b207f4d184c36147fce7ab40a531
Francis Tseng <ftzeng@gmail.com>
2014-03-30 01:04:20 -0400

    the flask app logger will email 500 errors

--- argos/web/app.py
+++ argos/web/app.py
@@ -10,5 +10,22 @@ app = Flask(__name__,
 
 app.config.update(APP)
 
+if not APP['DEBUG']:
+    # Email Flask application exceptions.
+    mh = handlers.SMTPHandler(
+            (APP['EMAIL_HOST'], APP['EMAIL_PORT']),
+            APP['EMAIL_HOST_USER'],
+            APP['ADMINS'],
+            'Argos Error :(',
+            credentials=(
+                APP['EMAIL_HOST_USER'],
+                APP['EMAIL_HOST_PASSWORD']
+            ),
+            secure=()
+    )
+    mh.setLevel(logging.ERROR)
+    app.logger.addHandler(mh)
+
+
 if environ.get('FLASK_ENV') == 'TESTING':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://argos_user:password@localhost:5432/argos_test'
