9dbe9b1d9e993597add2ee4868738eaa4a295757
Francis Tseng <ftzeng@gmail.com>
2014-04-03 17:23:50 -0400

    added foundation for event pages

--- argos/web/front/main.py
+++ argos/web/front/main.py
@@ -5,3 +5,7 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.jade')
+
+@bp.route('/event')
+def event():
+    return render_template('event.jade')
