eda54af4e72399124239b8e2155e62154e7b7c97
Francis Tseng <ftzeng@gmail.com>
2014-04-03 19:51:52 -0400

    converting static sketches into actual templates

--- argos/web/front/main.py
+++ argos/web/front/main.py
@@ -1,11 +1,36 @@
-from flask import Blueprint, render_template
+import argos.web.models as models
+
+from datetime import datetime
+
+import humanize
+
+from flask import Blueprint, request, render_template
 
 bp = Blueprint('main', __name__)
 
-@bp.route('/')
-def index():
-    return render_template('index.jade')
+PER_PAGE = 20
 
-@bp.route('/event')
-def event():
-    return render_template('event.jade')
+@bp.route('/')
+@bp.route('/latest')
+def index():
+    page = request.args.get('page', 1)
+    events = models.Event.query.paginate(page, per_page=PER_PAGE).items
+    return render_template('index.jade', events=events)
+
+@bp.route('/events/<int:id>')
+def events(id):
+    event = models.Event.query.get(id)
+    return render_template('event.jade', event=event)
+
+@bp.app_template_filter()
+def naturaltime(dt):
+    """
+    A tempalte filter for rendering
+    datetimes in human-readable form,
+    e.g. "1 hour ago", "5 days ago", etc.
+
+    Example usage (in `pyjade`)::
+
+        div= event.updated_at|naturaltime
+    """
+    return humanize.naturaltime(datetime.utcnow() - dt)
