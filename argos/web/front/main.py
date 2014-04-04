a752720d4c0a6092c1afefeb72e23c44bed26cf3
Francis Tseng <ftzeng@gmail.com>
2014-04-03 22:32:29 -0400

    added /watching and /trending routes, closes #95

--- argos/web/front/main.py
+++ argos/web/front/main.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 import humanize
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask.ext.security import current_user
 
 bp = Blueprint('main', __name__)
 
@@ -12,15 +13,39 @@ PER_PAGE = 20
 
 @bp.route('/')
 @bp.route('/latest')
-def index():
+def latest():
     page = request.args.get('page', 1)
     events = models.Event.query.paginate(page, per_page=PER_PAGE).items
-    return render_template('index.jade', events=events)
+    return render_template('events/collection.jade', events=events, title='The latest events')
+
+@bp.route('/trending')
+def trending():
+    page = request.args.get('page', 1)
+    events = models.Event.query.order_by(models.Event._score.desc()).paginate(page, per_page=PER_PAGE).items
+    return render_template('events/collection.jade', events=events, title='The latest, most shared events')
 
 @bp.route('/events/<int:id>')
 def events(id):
     event = models.Event.query.get(id)
-    return render_template('event.jade', event=event)
+    return render_template('events/member.jade', event=event)
+
+@bp.route('/watching')
+def watching():
+    if current_user.is_authenticated():
+        events = models.Event.query.join(models.Event.stories).filter(models.Event.stories.any(models.Story.id.in_([story.id for story in current_user.watching]))).all()
+        return render_template('events/collection.jade', events=events, title='The latest from stories you\'re watching', empty_msg='You aren\'t watching any stories!')
+    else:
+        flash('You need to be logged in')
+        return redirect(url_for('security.login'))
+
+@bp.route('/bookmarked')
+def bookmarked():
+    if current_user.is_authenticated():
+        events = current_user.bookmarked
+        return render_template('events/collection.jade', events=events, title='Your bookmarked events', empty_msg='You haven\'t bookmarked anything!')
+    else:
+        flash('You need to be logged in')
+        return redirect(url_for('security.login'))
 
 @bp.app_template_filter()
 def naturaltime(dt):
