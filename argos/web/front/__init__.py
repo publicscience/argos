0bde463bb1c0ec2bac8a8c7def4b1144a3a12aa5
Francis Tseng <ftzeng@gmail.com>
2014-04-05 17:37:23 -0400

    explicit about templates and static folders for front

--- argos/web/front/__init__.py
+++ argos/web/front/__init__.py
@@ -4,8 +4,15 @@ from argos.web.front.assets import assets
 
 from flask.ext.babel import Babel
 
+import os
+
 def create_app(**config_overrides):
     app = web.create_app(__name__, __path__, **config_overrides)
+
+    tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.template_folder = tmpl_dir
+    app.static_folder = static_dir
 
     oauth.init_app(app)
     assets.init_app(app)
