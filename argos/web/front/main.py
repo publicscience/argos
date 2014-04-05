dbc9cff7c724d2e061144573f3f5f6ce39e9d572
Francis Tseng <ftzeng@gmail.com>
2014-04-05 18:02:13 -0400

    feeds will now only show events which are not orphaned (i.e. they belong to at least one story)

--- argos/web/front/main.py
+++ argos/web/front/main.py
@@ -9,13 +9,17 @@ PER_PAGE = 20
 @bp.route('/latest')
 def latest():
     page = request.args.get('page', 1)
-    events = models.Event.query.paginate(page, per_page=PER_PAGE).items
+    # Filter to only show events which have been clustered
+    # (i.e. they belong to at least one story)
+    events = models.Event.query.filter(models.Event.stories).paginate(page, per_page=PER_PAGE).items
     return render_template('events/collection.jade', events=events, title='The latest events')
 
 @bp.route('/trending')
 def trending():
     page = request.args.get('page', 1)
-    events = models.Event.query.order_by(models.Event._score.desc()).paginate(page, per_page=PER_PAGE).items
+    # Filter to only show events which have been clustered
+    # (i.e. they belong to at least one story)
+    events = models.Event.query.filter(models.Event.stories).order_by(models.Event._score.desc()).paginate(page, per_page=PER_PAGE).items
     return render_template('events/collection.jade', events=events, title='The latest, most shared events')
 
 @bp.route('/events/<int:id>')
--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 from flask.ext.script import Manager, Shell, Server
 
 from argos.web import api
-from manage import PonderCommand, CreateSourcesCommand, CreateClientCommand, SeedCommand, ProfileCommand, EvaluateCommand
+from manage import PonderCommand, CreateSourcesCommand, CreateClientCommand, SeedCommand, ProfileCommand, EvaluateCommand, ClusterArticlesCommand, ClusterEventsCommand
 
 manager = Manager(api.create_app())
 manager.add_command('ponder', PonderCommand())
 manager.add_command('create:sources', CreateSourcesCommand())
 manager.add_command('create:client', CreateClientCommand())
+manager.add_command('cluster:articles', ClusterArticlesCommand())
+manager.add_command('cluster:events', ClusterEventsCommand())
 manager.add_command('profile', ProfileCommand())
 manager.add_command('evaluate', EvaluateCommand())
 manager.add_command('seed', SeedCommand())
