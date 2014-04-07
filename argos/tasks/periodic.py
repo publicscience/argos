03365b114adbe1d51a58a299c0d259ba37905ddc
Francis Tseng <ftzeng@gmail.com>
2014-04-07 10:40:47 -0400

    added some notifications for celery tasks

--- argos/tasks/periodic.py
+++ argos/tasks/periodic.py
@@ -1,10 +1,14 @@
-from argos.tasks import celery
+from argos.tasks import celery, notify
 
 from argos.core.models import Source, Article, Event
 from argos.core.membrane import collector
 from argos.datastore import db
 
 from datetime import datetime, timedelta
+
+# Logging.
+from argos.util.logger import logger
+logger = logger(__name__)
 
 @celery.task
 def collect():
@@ -26,9 +30,14 @@ def collect():
         collector.collect(source)
         source.updated_at = datetime.utcnow()
 
+    except Exception:
+        logger.exception('Exception while collecting for source {0}'.format(source.name))
+        raise
+
     finally:
         source.updating = False
         db.session.commit()
+        notify('Collecting for source {0} is complete.'.format(source.name))
 
 @celery.task
 def cluster_articles(batch_size=20, threshold=0.05):
@@ -38,6 +47,7 @@ def cluster_articles(batch_size=20, threshold=0.05):
     """
     articles = Article.query.filter(~Article.events.any()).limit(batch_size).all()
     Event.cluster(articles, threshold=threshold)
+    notify('Clustering articles successful.')
 
 @celery.task
 def cluster_events(batch_size=20, threshold=0.05):
@@ -47,3 +57,4 @@ def cluster_events(batch_size=20, threshold=0.05):
     """
     events = Event.query.filter(~Event.stories.any()).limit(batch_size).all()
     Story.cluster(events, threshold=threshold)
+    notify('Clustering events successful.')
