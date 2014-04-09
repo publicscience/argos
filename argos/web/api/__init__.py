e1534170eb8832add2de33fd7d2c8d64666e27ef
Francis Tseng <ftzeng@gmail.com>
2014-04-09 14:03:33 -0400

    fixed api templates route

--- argos/web/api/__init__.py
+++ argos/web/api/__init__.py
@@ -3,10 +3,15 @@ from argos.web.api.oauth import oauth
 
 from flask.ext.restful import Api
 
+import os
+
 api = Api()
 
 def create_app(**config_overrides):
     app = web.create_app(__name__, __path__, **config_overrides)
+
+    tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    app.template_folder = tmpl_dir
 
     api.init_app(app)
     oauth.init_app(app)
