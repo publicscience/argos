58a5ac3a081d2ead34da8c24d4564df9bfc3966d
Francis Tseng <ftzeng@gmail.com>
2014-04-16 11:16:34 -0400

    cleaned up wikidigester, no longer using it (dbpedia instead)

--- manage/profile.py
+++ manage/profile.py
@@ -8,8 +8,6 @@ and identify bottlenecks.
 
 import cProfile, pstats
 
-from argos.core.digester.wikidigester import WikiDigester
-
 from argos.datastore import db
 
 from argos.core.brain import vectorize
@@ -26,22 +24,6 @@ class ProfileCommand(Command):
     """
     def run(self):
         profile()
-
-def profile_wikidigester():
-    # Create a WikiDigester
-    part = 'http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles1.xml-p000000010p000010000.bz2'
-    w = WikiDigester('/tmp/wikitest.xml.bz2', db='wikitest', url=part)
-
-    w.purge()
-
-    print('Profiling WikiDigester...')
-
-    p = cProfile.Profile()
-    p.runctx('w.digest()', None, {'w': w})
-    ps = pstats.Stats(p)
-
-    # See which specific func takes the most time.
-    ps.sort_stats('time').print_stats(10)
 
 def profile_vectorize():
     print('argos.core.brain.vectorize')
