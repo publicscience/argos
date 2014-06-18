5f0030fe6ed9c85db9dcebcfd47beb811d77cc4c
Francis Tseng <f+accounts@frnsys.com>
2014-06-18 16:49:06 -0400

    argos theme for docs

--- doc/conf.py
+++ doc/conf.py
@@ -13,8 +13,7 @@
 
 import sys, os
 
-# Read the Docs theme
-import sphinx_rtd_theme
+import argos_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -98,7 +97,7 @@ exclude_patterns = ['_build']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'argos_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -106,7 +105,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [argos_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
