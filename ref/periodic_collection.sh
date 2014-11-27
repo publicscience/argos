b630fbcd9e9f74b34f95356b0039cf5848c11537
Francis Tseng <f+accounts@frnsys.com>
2014-11-27 13:14:29 -0500

    script fix

--- ref/periodic_collection.sh
+++ ref/periodic_collection.sh
@@ -3,4 +3,4 @@ source /env/argos/bin/activate
 
 # run celerybeat which periodically runs tasks.
 # the tasks that are run are defined in argos/conf/default/celery.py
-celery beat --app=argos.tasks.celery --schedule=/var/lib/celery/beat.db --pidfile=
+celerybeat --app=argos.tasks.celery --schedule=/var/lib/celery/beat.db --pidfile=
