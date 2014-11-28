23fd7f05690c1bf61937aa9387d3c433eadbcb3f
Francis Tseng <f@frnsys.com>
2014-11-28 05:45:33 +0000

    updated celery beat stuff

--- ref/periodic_collection.sh
+++ ref/periodic_collection.sh
@@ -1,6 +1,10 @@
 # source your argos virtualenv
 source /env/argos/bin/activate
 
+# create celerybeat schedule folder
+sudo mkdir -p /var/lib/celery/
+# you should probably chown this dir to whatever user is running celery
+
 # run celerybeat which periodically runs tasks.
 # the tasks that are run are defined in argos/conf/default/celery.py
-celerybeat --app=argos.tasks.celery --schedule=/var/lib/celery/beat.db --pidfile=
+celery beat --app=argos.tasks.celery --schedule=/var/lib/celery/beat.db --pidfile=
