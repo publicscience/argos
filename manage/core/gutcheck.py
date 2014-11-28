7111f891a617fcd49b1971ff19251af79320bbae
Francis Tseng <f@frnsys.com>
2014-11-28 05:51:20 +0000

    additional gutchecks

--- manage/core/gutcheck.py
+++ manage/core/gutcheck.py
@@ -1,9 +1,11 @@
 from flask.ext.script import Command
 
-from argos.core.models import Article, Event, Story
+from argos.core.models import Article, Event, Story, Source, Feed
 
 class GutcheckCommand(Command):
     def run(self):
         print('There are {0} articles.'.format(Article.query.count()))
         print('There are {0} events.'.format(Event.query.count()))
         print('There are {0} stories.'.format(Story.query.count()))
+        print('There are {0} sources.'.format(Source.query.count()))
+        print('There are {0} feeds.'.format(Feed.query.count()))
