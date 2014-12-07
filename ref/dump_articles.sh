11d8739d5bff891a54e8409dce5465cf359a5be7
Francis Tseng <f+accounts@frnsys.com>
2014-12-07 03:43:20 -0500

    updated dump articles

--- ref/dump_articles.sh
+++ ref/dump_articles.sh
@@ -1,4 +1,7 @@
-pg_dump -U argos_user -h localhost -d argos_dev -t article > article_dump.sql
-
-# to import:
-# pg_restore -d dbname filename
+# Dumps the remote database and loads it locally.
+ssh user@remote.co 'pg_dump -U argos_user -h localhost -d argos_dev -f ~/argos_dump.sql'
+scp user@remote.co:~/argos_dump.sql ~/
+dropdb argos_dev
+createdb -U argos_user -E utf8 -O argos_user argos_dev -T template0
+psql -U argos_user -d argos_dev -f ~/argos_dump.sql
+rm ~/argos_dump.sql
